7bb94371d73636df81b4eef42ee27fbc2375cb5a
Eduardo <00044923@uca.edu.sv>
2026-05-18 14:26:21 -0600

    feat: Librerías actualizadas para Navegacion 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -55,4 +57,10 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+
+    implementation(libs.androidx.navigation3.runtime)
+    implementation(libs.androidx.navigation3.ui)
+    implementation(libs.kotlinx.serialization.core)
+    implementation("io.coil-kt:coil-compose:2.7.0")
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
 }
